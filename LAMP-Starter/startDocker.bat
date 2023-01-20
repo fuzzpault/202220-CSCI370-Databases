eff2bb9aff6b8dab57aae8cb31817f9bddc3b4c2
Paul Talaga <talagap@uindy.edu>
2023-01-20 10:37:41 -0500

    rm added

--- LAMP-Starter/startDocker.bat
+++ LAMP-Starter/startDocker.bat
@@ -1,1 +1,1 @@
-docker run -i -t -p 80:80 -p 3306:3306 -v "%~dp0/app:/app" -v "%~dp0/mysql:/var/lib/mysql" -v "%~dp0/logs:/var/log/apache2" mattrayner/lamp:0.8.0-1804-php8
+docker run -i --rm -t -p 80:80 -p 3306:3306 -v "%~dp0/app:/app" -v "%~dp0/mysql:/var/lib/mysql" -v "%~dp0/logs:/var/log/apache2" mattrayner/lamp:0.8.0-1804-php8
